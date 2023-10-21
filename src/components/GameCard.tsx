78503cff92bb9ec065e6550cdc944a3089767045
Sai Teja Ambari <saiteja.ambari@gmail.com>
2023-10-20 22:17:03 -0400

    Game Card

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,15 @@
+import { Card, Heading, Image } from "@chakra-ui/react";
+import { Game } from "../hooks/useGames";
+
+interface Props{
+    game : Game;
+}
+const GameCard = ({game} : Props) => {
+return (
+<Card borderRadius={"10px"} overflow={"hidden"} >
+<Image src = {game.background_image}></Image>
+<Heading fontSize={"2xl"}>{game.name}</Heading>
+</Card>);
+}
+
+export default GameCard;
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,18 +1,19 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
-import { Text } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
 
 const GameGrid = () => {
-    const {games, errors} = useGames();
+  const { games, errors } = useGames();
   return (
     <>
       {errors && <Text>{errors}</Text>}
-      <ul>
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={10}>
         {games.slice(0, 100).map((game) => (
-          <li key={game.id}>{game.name}</li>
+          <GameCard key={game.id} game={game}></GameCard>
         ))}
-      </ul>
+      </SimpleGrid>
     </>
   );
 };
