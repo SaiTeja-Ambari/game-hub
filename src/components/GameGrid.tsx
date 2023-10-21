c5163e05b487080439927fe64ca137e16ad6d7df
Sai Teja Ambari <saiteja.ambari@gmail.com>
2023-10-21 15:10:53 -0400

    added container to void duplication of code

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -4,6 +4,7 @@ import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
   const { games, errors, loading } = useGames();
@@ -14,11 +15,15 @@ const GameGrid = () => {
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={10}>
         {loading &&
           skeletons.map((skeleton) => (
+            <GameCardContainer>
             <GameCardSkeleton key={skeleton}></GameCardSkeleton>
+            </GameCardContainer>
           ))}
 
         {games.slice(0, 100).map((game) => (
+            <GameCardContainer>
           <GameCard key={game.id} game={game}></GameCard>
+          </GameCardContainer>
         ))}
       </SimpleGrid>
     </>
