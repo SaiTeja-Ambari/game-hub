9cdcc1ff4b031c02871ff886aa57f87cb0bc8efe
Sai Teja Ambari <saiteja.ambari@gmail.com>
2023-10-28 19:15:24 -0400

    no image and box

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import { Grid, GridItem, HStack, Show } from "@chakra-ui/react";
+import { Box, Flex, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
@@ -40,15 +40,18 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
-        <HStack spacing = {5} paddingLeft={2} marginBottom={5}>
+        <Flex paddingLeft={2} marginBottom={5}>
+          <Box marginRight = {5}>
           <PlatformSelector
             selectedPlatform={gameQuery?.platform}
             onSelectPlatForm={(platform) =>
               setGameQuery({ ...gameQuery, platform })
             }
           ></PlatformSelector>
+          </Box>
           <SortSelector sortOrder = {gameQuery.sortOrder} onSelectSortOrder = {(sortOrder) => setGameQuery({...gameQuery, sortOrder})}></SortSelector>
-        </HStack>
+          
+        </Flex>
         <GameGrid gameQuery={gameQuery}></GameGrid>
       </GridItem>
     </Grid>
--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,6 @@
+import noimage from '../assets/no-image-available-like-missing-picture.jpg'
 const getCroppedImageURL = (url : string) => {
-    if(!url) return '';
+    if(!url) return noimage;
     const index = url.indexOf('media/') + 'media/'.length ;
     return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
 }
