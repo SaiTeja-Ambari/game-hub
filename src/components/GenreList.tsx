3066b90a884bc18187674caf3dbf86364ba39091
Sai Teja Ambari <saiteja.ambari@gmail.com>
2023-10-28 11:33:58 -0400

    left panel spinner

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,23 +1,28 @@
-import { HStack, List, ListItem, Image, Text } from "@chakra-ui/react";
+import { HStack, List, ListItem, Image, Text, Spinner } from "@chakra-ui/react";
 import useGenre from "../hooks/useGenre";
 import getCroppedImageURL from "../services/image-url";
 
 const GenreList = () => {
-  const { Datas } = useGenre();
+  const { Datas, loading, errors
+   } = useGenre();
+
+  if (loading) return <Spinner />;
+  if(errors) return null;
+
   return (
     <List>
       {Datas.map((genre) => (
-        <ListItem key={genre.id} paddingY='5px'>
-        <HStack>
-          <Image
-            boxSize="32px"
-            borderRadius={8}
-            src={getCroppedImageURL(genre.image_background)}
-          />
-          <Text fontSize='lg'>{genre.name}</Text>
-        </HStack>
-      </ListItem>
-    ))}
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageURL(genre.image_background)}
+            />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
+      ))}
     </List>
   );
 };
